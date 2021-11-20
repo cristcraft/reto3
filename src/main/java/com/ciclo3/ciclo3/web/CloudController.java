fe4d7faa10e7cbcc57ecb0dbbc9684ecf601b37a
cristcraft <cristmateus29@gmail.com>
2021-11-19 19:28:38 -0500

    target2

--- src/main/java/com/ciclo3/ciclo3/web/CloudController.java
+++ src/main/java/com/ciclo3/ciclo3/web/CloudController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/cloud")
+@RequestMapping("/api/Cloud")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class CloudController{
     
